321da27abb1dc4891d4b289f8698207d571c90be
JoeDGit <joe.dobbins89@gmail.com>
2022-06-26 20:26:18 +0100

    Progress on the movies section, added loading and got the page rendering 3 pages of results from the Api using my callApi function and a promise.all Ended up saving alot of code after all. Still need to add the page reload on endpoint change and some styling for the links, along with the active class to show which is active. Alot of this code should be transferable to Tv shows which is good. The end is in sight.

---
+++ src/Movies/MovieLoader.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MovieLoader() {
+  let circleCommonClasses =
+    "flex bg-white items-center h-[10px] w-[10px] rounded-full ";
+
+  return (
+    <div className="child:mt-[9rem] justify-center align-center basis h-full flex w-100">
+      <div className="relative bottom-[50px] left-[75px] text-white text-3xl">
+        Loading
+      </div>
+      <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
+      <div className={`${circleCommonClasses} mr-1 animate-bounce200`}></div>
+      <div className={`${circleCommonClasses} animate-bounce400`}></div>
+    </div>
+  );
+}
--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,32 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { callApi } from "../utils/callApi";
 import Nav from "../Home/Nav";
-import MoviesCard from "./MoviesCard";
-
-const api_key = "fd4e5f51938f96d0f16bfb76bed86942";
+import Card from "../Card/Card";
+import MovieLoader from "./MovieLoader";
 
 export default function Movies() {
-  const [movies, setMovies] = useState({});
+  const [movies, setMovies] = useState();
   const [endpoint, setEndpoint] = useState("now_playing");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const item = await fetch(
-        `https://api.themoviedb.org/3/movie/${endpoint}?api_key=${api_key}&language=en-US&page=1`
-      ).then((pageResponse) => pageResponse.json());
-      setMovies(item);
+    const fetchMovies = async (props) => {
+      Promise.all([
+        callApi("movie", "popular"),
+        callApi("movie", "popular", "&language=en-US", "&page=2"),
+        callApi("movie", "popular", "&language=en-US", "&page=3"),
+      ]).then((result) => {
+        const sortedApiCall = result.flat().sort(
+          (x, y) => {
+            return y[props] - x[props];
+          },
+          [props]
+        );
+        setMovies(sortedApiCall);
+        setIsLoading(false);
+      });
     };
-    fetchMovies();
+    fetchMovies("rating");
   }, [endpoint]);
 
   const handleClick = (id) => {
     setEndpoint(id);
   };
+
   return (
     <React.Fragment>
       <Nav />
-      <div className="w-100 flex">
-        <div className="rounded  ml-2 p-2 flex ">
-          <ul className="child:cursor-pointer flex flex-col mt-4  md:flex-start md:flex-row md:space-x-8 md:mt-0 md:text-medium md:font-medium ">
+
+      <div className="w-100 flex justify-center items-center ">
+        <div className="  ">
+          <ul className="child:ring-2 child:rounded text-white child:cursor-pointer flex flex-col mt-4  md:flex-start md:flex-row md:space-x-8 md:mt-0 md:text-medium md:font-medium ">
             <li>
               <a onClick={() => handleClick("now_playing")}>In cinemas</a>
             </li>
@@ -41,8 +54,27 @@ export default function Movies() {
             </li>
           </ul>
         </div>
-        <MoviesCard movies={movies} />
       </div>
+      {isLoading ? (
+        <MovieLoader />
+      ) : (
+        <div className="container  flex flex-wrap justify-center min-w-full text-center mt-5 p-2">
+          {movies.map((result, index) => {
+            return (
+              <Card
+                key={`Card-${index + 1}`}
+                title={result.title}
+                rating={result.rating}
+                poster={result.poster}
+                release={result.releaseDate.substr(0, 4)}
+                type={result.type}
+                imdbId={result.imdb_id}
+                trailer={result.trailer}
+              />
+            );
+          })}
+        </div>
+      )}
     </React.Fragment>
   );
 }
