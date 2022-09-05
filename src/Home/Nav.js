04d8e8fca2ff425eca6489d3276dbd39a806a0bf
JoeDGit <joe.dobbins89@gmail.com>
2022-09-05 20:56:47 +0100

    Fixed active nav link highlighting using react router. Got rid of some old development code

--- src/Home/Nav.js
+++ src/Home/Nav.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../index.css";
 import flixlogo from "!file-loader!./flixlogo.svg";
+import { NavLink } from "react-router-dom";
 
 const getWindowWidth = () => {
   const width = window.innerWidth;
@@ -9,20 +10,6 @@ const getWindowWidth = () => {
 
 export default function Nav() {
   const [menuActive, setMenuActive] = useState(false);
-
-  const [activeNav, setActiveNav] = useState("true");
-  const [windowSize, setWindowSize] = useState(getWindowWidth());
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize(getWindowWidth);
-    };
-    if (windowSize > 766) {
-      setMenuActive(true);
-    }
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  });
 
   const handleMenuClick = () => {
     if (menuActive) {
@@ -33,13 +20,8 @@ export default function Nav() {
   };
 
   const standardStyle =
-    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
-  const activeStyle =
-    "block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+    "block py-2 pr-4 pl-3 border-b border-gray-700 hover:bg-gray-50  md:border-0 md:p-0 md:pb-1 md:hover:text-white text-gray-400 hover:bg-gray-700 md:hover:text-white md:hover:bg-transparent ";
 
-  const handleClick = (navlink) => {
-    setActiveNav(navlink);
-  };
   return (
     <div className="w-full border-b-[1px] border-gray-400">
       <nav className=" border-gray-200 px-2 sm:px-4 pt-2.5  md:pb-2.5 md:pt-1.5 mb-2 md:mb-0 mx-4 rounded   ">
@@ -88,58 +70,77 @@ export default function Nav() {
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"></div>
               </div>
 
-              <ul className="flex flex-col mt-4  md:flex-start md:flex-row md:space-x-8 md:mt-0 md:text-medium md:font-medium ">
-                <li>
-                  <a
-                    href="/"
-                    onClick={() => handleClick("home")}
-                    className={
-                      activeNav === "home" ? activeStyle : standardStyle
-                    }
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/movies"
-                    onClick={() => handleClick("movies")}
-                    className={
-                      activeNav === "movies" ? activeStyle : standardStyle
-                    }
-                  >
-                    Movies
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/tv"
-                    onClick={() => handleClick("tvshows")}
-                    className={
-                      activeNav === "tvshows" ? activeStyle : standardStyle
-                    }
-                  >
-                    TV Shows
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/randomiser"
-                    onClick={() => handleClick("randomiser")}
-                    className={
-                      activeNav === "randomiser" ? activeStyle : standardStyle
-                    }
-                  >
-                    Randomiser
-                  </a>
-                </li>
-              </ul>
+              <nav className="flex flex-col mt-4  md:flex-start md:flex-row md:space-x-8 md:mt-0 md:text-medium md:font-medium ">
+                <NavLink
+                  exact
+                  to="/"
+                  className={standardStyle}
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "white",
+                          textDecoration: "underline",
+                          textUnderlineOffset: "3px",
+                        }
+                      : { color: "rgb(156 163 175)" }
+                  }
+                >
+                  Home
+                </NavLink>
+
+                <NavLink
+                  exact
+                  to="/movies"
+                  className={standardStyle}
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "white",
+                          textDecoration: "underline",
+                          textUnderlineOffset: "3px",
+                        }
+                      : { color: "rgb(156 163 175)" }
+                  }
+                >
+                  Movies
+                </NavLink>
+
+                <NavLink
+                  exact
+                  to="/tv"
+                  className={standardStyle}
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "white",
+                          textDecoration: "underline",
+                          textUnderlineOffset: "3px",
+                        }
+                      : { color: "rgb(156 163 175)" }
+                  }
+                >
+                  TV Shows
+                </NavLink>
+
+                <NavLink
+                  exact
+                  to="/randomiser"
+                  className={standardStyle}
+                  style={({ isActive }) =>
+                    isActive
+                      ? {
+                          color: "white",
+                          textDecoration: "underline",
+                          textUnderlineOffset: "3px",
+                        }
+                      : { color: "rgb(156 163 175)" }
+                  }
+                >
+                  Randomiser
+                </NavLink>
+              </nav>
             </div>
           )}
-          {/* <div className="hidden md:flex child:px-8 child:py-1 child:rounded child:bg-gray-200 gap-3">
-            <button>Register</button>
-            <button>Login</button>
-          </div> */}
         </div>
       </nav>
     </div>
